16bdec7b52a164c64a3a2591018e6187f81fb81d
Mike-Beyt <beytisamichael@gmail.com>
2024-12-05 15:42:42 +0330

    added grid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,34 @@
 'use client';
 import React from 'react';
-import { useTheme } from 'next-themes';
 
-import { Button } from '@/components/ui/button';
-import { Input } from '@/components/ui/input';
+import Grid from '@/components/common/grid.component';
 
 export default function Home() {
-  const { setTheme } = useTheme();
   return (
-    <div className="p-5">
-      <div className="m-5 text-3xl font-medium">
-        - apply fonts <br />- apply colors
-      </div>
-      <div className="m-5 flex gap-2">
-        <Button onClick={() => setTheme('dark')}>dark</Button>
-        <Button onClick={() => setTheme('light')}>light</Button>
-        <Button onClick={() => setTheme('system')}>system</Button>
-      </div>
-      <div className="m-5">
-        <Input className="w-44" />
-      </div>
-      <div className="h-20 w-20 bg-color-stroke">test colors</div>
+    <div className="p-8">
+      <Grid cols={12} className="h-auto w-full">
+        <div className="col-span-9 h-auto w-full">
+          <Grid cols={4} className="mt-4 h-[268px]">
+            <div className="col-span-2 bg-red-200">2/4</div>
+            <div className="col-span-1 bg-red-100">1/4</div>
+            <div className="col-span-1 bg-red-300">1/4</div>
+          </Grid>
+
+          <Grid cols={12} className="mt-4 h-[268px] w-full">
+            <div className="col-span-2 bg-slate-200">2/12</div>
+            <div className="col-span-6 bg-slate-300">6/12</div>
+            <div className="col-span-4 bg-slate-400">4/12</div>
+          </Grid>
+
+          <Grid cols={12} className="mt-4 h-[268px] w-full">
+            <div className="col-span-5 bg-blue-300">5/12</div>
+            <div className="col-span-4 bg-blue-400">4/12</div>
+            <div className="col-span-3 bg-blue-500">3/12</div>
+          </Grid>
+        </div>
+
+        <div className="col-span-3">Sidebar</div>
+      </Grid>
     </div>
   );
 }
