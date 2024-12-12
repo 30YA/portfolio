3bece584a011ef060158d458086f08b848b11949
siavash <siavash.aghazadeh@gmail.com>
2024-12-13 01:10:53 +0330

    feat : about page

---
+++ src/components/common/slider/slider.component.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Pagination } from 'swiper/modules';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+const whatIdoMock = [
+  {
+    title: 'Art Direction',
+    description:
+      'Get design tips & guid straight to your inbox for free! Get design tip & guid straight to your inbox for free!',
+  },
+  {
+    title: 'Art Direction',
+    description:
+      'Get design tips & guid straight to your inbox for free! Get design tip & guid straight to your inbox for free!',
+  },
+  {
+    title: 'Art Direction',
+    description:
+      'Get design tips & guid straight to your inbox for free! Get design tip & guid straight to your inbox for free!',
+  },
+];
+
+function Slider() {
+  const pagination = {
+    clickable: true,
+    renderBullet: function (index: number, className: string) {
+      return `<span class="${className} bg-color-title/20 h-4 w-4"></span>`;
+    },
+  };
+  return (
+    <Swiper
+      pagination={pagination}
+      modules={[Pagination]}
+      className="mySwiper about-swiper !relative mb-5"
+    >
+      {whatIdoMock.map((item, index) => (
+        <SwiperSlide key={index} className="cursor-grab">
+          <h3 className="text-[26px] font-semibold text-color-title">
+            {item.title}
+          </h3>
+          <p className="mb-7 mt-4 text-xl font-normal text-color-title">
+            {item.description}
+          </p>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  );
+}
+
+export default Slider;
